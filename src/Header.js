9a85c37137adaba72a39a763b620d12d18e2a890
Roman Rios <romanrios@live.com>
2023-12-28 23:26:31 -0300

    fixes

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 export default function Header(props) {
     return (<div id="cabecera" className="App-header">
-        <img className="logo" src="/logo.svg" alt="Logo" />
+        <img className="logo" src={`${process.env.PUBLIC_URL}/logo.svg`} alt="Logo" />
         <h3 className="mensaje">Bienvenido a la página de Román Ríos</h3>
     </div>)
 }
